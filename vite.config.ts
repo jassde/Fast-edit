159307abf75841ea11b6d9b46f9bdf30598ae9f5
jassde <59785413+jassde@users.noreply.github.com>
2026-07-05 09:24:53 +0200

    ff

--- vite.config.ts
+++ vite.config.ts
@@ -14,8 +14,8 @@ export default defineConfig(async () => ({
   clearScreen: false,
   // 2. tauri expects a fixed port, fail if that port is not available
   server: {
-    port: 1420,
-    strictPort: true,
+    port: parseInt(process.env.PORT || "1420"),
+    strictPort: false,
     host: host || false,
     hmr: host
       ? {
